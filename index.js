46cd3bff93c9d980382d6f2141ca03c40f5bd74e
DBric <d.bricaud99@gmail.com>
2020-09-22 12:03:28 +0200

    -fc state

--- index.js
+++ index.js
@@ -3,79 +3,86 @@ const Discord = require('discord.js');
 const Gif = require('./Models/gif.js');
 const fs = require('fs');
 
-const gifJson = fs.readFileSync('./Data/gif_content.json', function(err) {
-    if(err) {
+const gifJson = fs.readFileSync('./Data/gif_content.json', function (err) {
+    if (err) {
         console.log(err);
     }
 });
 const gif = new Gif(JSON.parse(gifJson));
 
-const bot = new Discord.Client({ disableEveryone: true});
+const bot = new Discord.Client({ disableEveryone: true });
 const dateFinale = new Date();
-
-bot.on('ready', async () => {
-    console.log(`${bot.user.username} est en ligne !`);
-    bot.user.setActivity('fait ses comptes');
-})
+let count = bot.setTimeout(() => { }, 1);
 
 let tempsRestant;
 let tempsRestantSec;
 let tempsRestantMin;
 let tempsRestantHours;
 
+bot.on('ready', async () => {
+    console.log(`${bot.user.username} est en ligne !`);
+    bot.user.setActivity("ses comptes", { type: "WATCHING" })
+})
+
+
 let timeToWait = 60000 * 5; //toutes les 5 mins
 
-function progress(message){
+function progress(message) {
     tempsRestant = dateFinale - Date.now();
-    tempsRestantSec = Math.floor(tempsRestant/1000) % 60;
-    tempsRestantMin = Math.floor(tempsRestant/60000);
+    tempsRestantSec = Math.floor(tempsRestant / 1000) % 60;
+    tempsRestantMin = Math.floor(tempsRestant / 60000);
     let txt = `Il reste ${tempsRestantMin} minutes et ${tempsRestantSec} secondes`;
-    if(tempsRestant <= 0) {
+    if (tempsRestant <= 0) {
         message.channel.send(gif.alea());
         timeToWait = 60000 * 5;
     } else {
         message.channel.send(txt);
-        if((tempsRestant / 1000) < 10) {//inf à 10sec
+        if ((tempsRestant / 1000) < 10) {//inf à 10sec
             timeToWait = 1000;
-        } else if((tempsRestant / 1000) < 60) {//inf à 1min
+        } else if ((tempsRestant / 1000) < 60) {//inf à 1min
             timeToWait = 1000 * 5;
-        } else if((tempsRestant / 1000) < 1200) {//inf à 20min
+        } else if ((tempsRestant / 1000) < 1200) {//inf à 20min
             timeToWait = 60000 * 1;
-        } else if((tempsRestant / 1000) < 3600) {//inf à 1h
+        } else if ((tempsRestant / 1000) < 3600) {//inf à 1h
             timeToWait = 60000 * 5;
         }
 
-        setTimeout(()=> {
+        bot.setTimeout(() => {
             progress(message);
         }, timeToWait);
     }
 }
 
 bot.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type == 'dm') return;
+    if (message.author.bot) return;
+    if (message.channel.type == 'dm') return;
 
     let prefix = config.prefix;
     let msgArray = message.content.split(" ");
     let cmd = msgArray[0];
     let args = msgArray.slice(1);
 
-    if(cmd != prefix) return
-    if(args[0] === "start"){
+    if (cmd != prefix) return
+    if (args[0] === "start") {
         console.log("start");
         dateFinale.setHours(args[1].split(':')[0], args[1].split(':')[1]);
-        setTimeout(()=> {
+        count = bot.setTimeout(() => {
             progress(message);
         }, 100);
         return message.channel.send("Démarrage");
-    } else if (args[0] == "addGif"){
+    } else if (args[0] == "addGif") {
         gif.content.push(args[1]);
         gif.save();
         return message.channel.send(`Ce gif vient d'être ajouter à l'ensemble de mes gifs : ${gif.lastAdd()}`)
+    } else if (args[0] == "state") {
+        tempsRestant = dateFinale - Date.now();
+        tempsRestantSec = Math.floor(tempsRestant / 1000) % 60;
+        tempsRestantMin = Math.floor(tempsRestant / 60000);
+        let txt = `Il reste ${tempsRestantMin} minutes et ${tempsRestantSec} secondes`;
+        return message.channel.send(txt);
     }
 
 })
-
 
 bot.login(config.token);
 
